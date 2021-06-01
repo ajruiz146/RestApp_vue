327a7d57780bd7e9a0966c2716ba970b434ea55b
MarioPadillaMolina <mario@dameuntoque.com>
2021-06-02 01:26:43 +0200

    style-staff_cards

--- src/components/Cards/StaffCards.vue
+++ src/components/Cards/StaffCards.vue
@@ -1,45 +1,233 @@
 <template>
-
-<div class="card shadow">
-  <div class="card-header bg-transparent">
-    <h3 class="mb-0">Staff</h3>
-    <div class="create-button">
-      <i class="ni ni-fat-add"></i>
+  <div class="card shadow">
+    <div class="card-header bg-transparent">
+      <h3 class="mb-0">Staff</h3>
+      <div class="create-button">
+        <i class="ni ni-fat-add"></i>
+      </div>
+      <div class="filters">
+        <select class="form-select form-select-sm" name="" id="">
+          <option value="">Select Staff</option>
+          <option value="">Waiters</option>
+          <option value="">Cookers</option>
+        </select>
+        <select class="form-select form-select-sm" name="" id="">
+          <option value="">Per Page</option>
+          <option value="">12</option>
+          <option value="">20</option>
+          <option value="">50</option>
+        </select>
+        <select class="form-select form-select-sm" name="" id="">
+          <option value="">Order by</option>
+          <option value="">Name Asc</option>
+          <option value="">Name Desc</option>
+        </select>
+      </div>
     </div>
-    <div class="filters">
-      <select class="form-select form-select-sm" name="" id="">
-        <option value="">Select Staff</option>
-        <option value="">Waiters</option>
-        <option value="">Cookers</option>
-      </select>
-      <select class="form-select form-select-sm" name="" id="">
-        <option value="">Per Page</option>
-        <option value="">12</option>
-        <option value="">20</option>
-        <option value="">50</option>
-      </select>
-      <select class="form-select form-select-sm" name="" id="">
-        <option value="">Order by</option>
-        <option value="">Name Asc</option>
-        <option value="">Name Desc</option>
-      </select>
+    <div class="card-body">
+      <div class="wrapper">
+        <div class="row staff">
+          <!-- inicio card -->
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#"><img src="img/icons/icon_edit.svg" alt="icon-edit"></a>
+                <a href="#"><img src="img/icons/icon_trash.svg" alt="icon-delete"></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#"><i class="ni ni-ruler-pencil text-black"></i></a>
+                <a href="#"><i class="ni ni-fat-remove text-black"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <div class="card-img-top" />
+            <div class="profile-thumb-block">
+              <img
+                src="https://randomuser.me/api/portraits/men/41.jpg"
+                alt="profile-image"
+                class="profile"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Manolito Gafotas</h5>
+              <p class="card-text">Waiter</p>
+              <div class="cards-buttons">
+                <a href="#" class="btn btn-primary edit">Edit</a>
+                <a href="#" class="btn btn-primary delete">Delete</a>
+              </div>
+            </div>
+          </div>
+          <!-- fin card -->
+        </div>
+      </div>
+    </div>
+    <div
+      class="card-footer d-flex justify-content-end"
+      :class="type === 'dark' ? 'bg-transparent' : ''"
+    >
+      <base-pagination total="40"></base-pagination>
     </div>
   </div>
-  <div class="card-body">
-    <div class="wrapper">
-      <!-- inicio card -->
-
-      <!-- fin card -->
-    </div>
-  </div>
-  <div
-    class="card-footer d-flex justify-content-end"
-    :class="type === 'dark' ? 'bg-transparent' : ''"
-  >
-    <base-pagination total="40"></base-pagination>
-  </div>
-</div>
-
 </template>
 
 <script>
@@ -48,13 +236,13 @@ import axios from "axios";
 export default {
   data() {
     return {
-      data: ''
+      data: "",
     };
   },
   mounted() {
     axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then(response => (this.data = response))
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => (this.data = response));
   },
 };
 </script>
@@ -78,18 +266,93 @@ export default {
 }
 
 select {
-    margin: 0 3px;
+  margin: 0 3px;
 }
 .filters {
-    display: flex;
-}  
-
-.wrapper {
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
 }
 
 /* A partir de aquí */
+.staff .card-img-top {
+  height: 110px;
+  width: 100%;
+  overflow: hidden;
+  background-color: #19746b;
+}
 
+.staff .card {
+  overflow: hidden;
+  height: 380px;
+  margin: 0.7em 0.7em;
+  padding: 0;
+  box-shadow: 1px 1px 4px rgb(128 128 128 / 29%);
+}
 
+.staff .card-body {
+  position: relative;
+  z-index: 9999;
+}
+
+.staff .card-body::before {
+  content: "";
+  display: block;
+  width: 114%;
+  height: 25%;
+  position: absolute;
+  top: -23px;
+  left: 2px;
+  transform: rotate(-7deg);
+  background: white;
+  z-index: -1;
+}
+
+.staff .profile {
+  border-radius: 50%;
+  position: absolute;
+  top: 30px;
+  left: 50%;
+  max-width: 100px;
+  opacity: 1;
+  box-shadow: 3px 3px 20px rgb(0 0 0 / 50%);
+  border: 2px solid rgba(255, 255, 255, 1);
+  -webkit-transform: translate(-50%, 0%);
+  transform: translate(-50%, 0%);
+  z-index: 99999;
+}
+
+.staff .card-title {
+  font-size: 25px;
+  padding-top: 22px;
+  text-align: center;
+}
+
+.staff .card-text {
+  text-align: center;
+}
+
+.staff .cards-buttons {
+  position: absolute;
+  bottom: 23px;
+  display: flex;
+  width: 238px;
+  justify-content: space-between;
+}
+
+.staff .cards-buttons .edit {
+  background-color:#898989;
+}
+
+.staff .cards-buttons .delete {
+  background-color:#898989;
+}
+
+.staff .cards-buttons a img {
+  width: 30px;
+  height: 30px;
+}
+
+.staff .text-black {
+  color: black;
+  font-size: 24px;
+}
 </style>
